68affebbd08f615af1114377219ebfc2d2372121
fadhili-ed <fadhilied00@gmail.com>
2020-03-04 15:02:53 +0300

    sending requests and getting responses while refactoring code on my localhost

--- week4/sinatra/myapp.rb
+++ week4/sinatra/myapp.rb
@@ -1,7 +1,41 @@
 require 'sinatra'
+
 get '/' do
-    "Hello World!"
+   erb :index
 end
 get '/:name' do
-    params.to_s
+    @name = params["name"].split("")
+    erb :myname_character
+    
 end
+get '/:name/signup' do
+    "I am trying to sign in "
+end 
+
+get '/:name/signup/:age' do
+    age = params["age"]
+    @age = age.to_i
+
+  erb :signup
+end
+
+get '/:name/signup/:age/people_details' do
+  @people_details = {
+        "name" => ["Alice", "Michael", "Jane", "Clare", "Mary", "John"],
+        "age" => [20, 23, 43, 32, 23, 44],
+        "country" => ["Kenya", "Tanzania", "Ghana", "Uganda", "Ethiopia", "Egypt"]
+    }
+    erb :people_details
+end
+__END__
+
+@@layout
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<%=yield%>
+</body>
+</html>
